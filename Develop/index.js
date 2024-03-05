ac6e5255698bfff506c4619d17e26b692b8551be
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-03-04 22:41:36 -0600

    Generated an array of questions that the user will be prompted with for the readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,58 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your Peoject?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Give a description of you project',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Provide a Table of Contents',
+        name: 'Table of Contents',
+    },
+    {
+        type: 'input',
+        message: 'Any Installation required?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Privide Instructions and examples for use',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'ANy liscense required?',
+        name: 'liscense',
+    },
+    {
+        type: 'input',
+        message: 'All Conributers to the Project',
+        name:'Contributing',
+    },
+    {
+        type: 'input',
+        message: 'Any Test?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'Describe Test.',
+        name: 'tests description',
+    },
+    {
+        type: 'input',
+        message: 'Any Questions?',
+        name: 'Questions',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
