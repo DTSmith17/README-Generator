49b9aca2ac7aa29508cc81519e37119f95120c23
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-03-06 18:28:08 -0600

    Update README questions with selction options. Added function to write README file. Added function to inialize app

---
+++ Develop/README.md
@@ -0,0 +1,14 @@
+# sdf
+## Description
+undefined
+## Table Of Contents
+undefined
+## Installations
+undefined
+## Usage
+sdf
+## Liscense
+undefined
+
+
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,14 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
+
 const questions = [
     {
         type: 'input',
-        message: 'What is the title of your Peoject?',
+        message: 'What is the title of your Project?',
         name: 'title',
     },
     {
@@ -13,52 +17,66 @@ const questions = [
         name: 'description',
     },
     {
-        type: 'input',
+        type: 'checkbox',
         message: 'Provide a Table of Contents',
-        name: 'Table of Contents',
+        name: 'TableOfContents',
+        choices: ['Installation', 'Usage', 'License', 'Constirbutors', 'Tests Description']
     },
     {
         type: 'input',
         message: 'Any Installation required?',
         name: 'installation',
+        when: (answer) => answer.TableOfContents.includes('Installation'),
     },
     {
         type: 'input',
-        message: 'Privide Instructions and examples for use',
+        message: 'How is this project to be used',
         name: 'usage',
+        when: (answer) => answer.TableOfContents.includes('usage'),
     },
     {
-        type: 'input',
-        message: 'ANy liscense required?',
-        name: 'liscense',
+        type: 'checkbox',
+        message: 'Any license required?',
+        name: 'license',
+        choices: ['MIT', 'Apache', 'Berkeley Source Distribution (BSD)', 'Unlicense', 'General Pblic License (GPL)', 'Affero GPL (AGPL)', 'Lesser General Public License (LGPL)', 'Eclipse Public License (EPL)','Mozilla Public License (MPL)'],
+        when: (answer) => answer.TableOfContents.includes('license'),
     },
     {
         type: 'input',
         message: 'All Conributers to the Project',
-        name:'Contributing',
-    },
-    {
-        type: 'input',
-        message: 'Any Test?',
-        name: 'tests',
+        name:'contributors',
+        when: (answer) => answer.TableOfContents.includes('contributors'),
     },
     {
         type: 'input',
         message: 'Describe Test.',
-        name: 'tests description',
+        name: 'testsDescription',
+        when: (answer) => answer.TableOfContents.includes('testsDescription'),
     },
     {
         type: 'input',
         message: 'Any Questions?',
-        name: 'Questions',
+        name: 'questions',
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    let markdownData = generateMarkdown(data);
+fs.writeFile(fileName, markdownData, (err) =>
+err ? console.error(err) : console.log('File Written')
+)
+}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        console.log(response);
+        writeToFile("README.md", response);
+    })
+
+}
 
 // Function call to initialize app
 init();
