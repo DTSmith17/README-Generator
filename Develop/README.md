5758921d23aabba2b1c66bcd2dac8bf4c133c7ad
DTSmith17 <153142161+DTSmith17@users.noreply.github.com>
2024-03-09 12:01:08 -0600

    Added return badges for every license selection.

---
+++ Develop/README.md
@@ -0,0 +1,18 @@
+# sdfsdfsdf
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+## Description
+fdsdfsdf
+## Table Of Contents
+Installation,License
+## Installations
+sdfsdf
+## Usage
+undefined
+## License 
+    MIT
+## Contributers
+undefined
+## Test Description
+undefined
+## Questions
+undefined
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,19 +2,38 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(licenses) {
   const badges = licenses.map((license) => {
-    if (license === 'MIT') {
-      return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    if (license === "MIT") {
+      return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
     }
-
-  })
-  return badges.join('');
+    if (license === "Apache") {
+      return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+    }
+    if (license === "BSD 3-Clause") {
+      return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+    }
+    if (license === "Unlicense") {
+      return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`;
+    }
+    if (license === "Eclipse Public License 1.0") {
+      return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+    }
+    if (license === "GNU GPL v3") {
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+    }
+    if (license === "IBM") {
+      return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`;
+    }
+    if (license === "Mozilla Public License (MPL)") {
+      return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+    }
+  });
+  return badges.join("");
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (!license) {
-    
   }
 }
 
@@ -22,10 +41,10 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (!license) {
-    return ''
+    return "";
   } else {
     return `## License 
-    .${license}.`
+    ${license}`;
   }
 }
 
